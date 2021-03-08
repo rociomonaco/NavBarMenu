13c6e07acc87612b5738da0e6abb2732dd5edb58
Rocio Monaco <rociomonaco@gmail.com>
2021-03-07 22:53:46 -0300

    modificacion de class

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,20 @@
 function NavBar() {
   return (
     <div className="App">
-      <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-          <h1 class="navbar-brand" >VITALI SHOES</h1>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <h1 className="navbar-brand" >VITALI SHOES</h1>
           
           
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row ">
-            <li class="nav-item">
-              <a class="nav-link px-2" aria-current="page" href="#home">Home</a>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row ">
+            <li className="nav-item">
+              <a className="nav-link px-2" aria-current="page" href="#home">Home</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link px-2" href="#store">Store</a>
+            <li className="nav-item">
+              <a className="nav-link px-2" href="#store">Store</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link px-2" href="#contact">Contact</a>
+            <li className="nav-item">
+              <a className="nav-link px-2" href="#contact">Contact</a>
             </li>
           </ul>
         </div>
